eea9384ebecc761706469d73c61c5db5e1c0c2f9
Gabriel Xavier <gabriel.xavier@sinapseinformatica.com.br>
2020-09-18 11:16:11 -0300

    Layout Create Task

--- app/src/main/java/com/example/focus_now/ui/fragment/CreateTask.kt
+++ app/src/main/java/com/example/focus_now/ui/fragment/CreateTask.kt
@@ -1,10 +1,9 @@
 package com.example.focus_now.ui.fragment
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.focus_now.R
 import com.example.focus_now.ui.viewmodel.StateAppViewModel
 import com.example.focus_now.ui.viewmodel.VisualComponents
@@ -18,11 +17,27 @@ class CreateTask : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_create_task, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        stateAppViewModel.hasComponent = VisualComponents(appBar = true, title = true, floatActionButton = false)
+        stateAppViewModel.hasComponent =
+            VisualComponents(appBar = true, title = true, floatActionButton = false)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.save_task -> {
+                Toast.makeText(context, "Salvar Tarefa", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_save_task, menu)
+
     }
 }
